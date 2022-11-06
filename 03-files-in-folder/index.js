12ff5aded929aefa3d149ae775949ef4f8591543
MaryKolosova <97162645+MaryKolosova@users.noreply.github.com>
2022-11-06 23:06:25 +0300

    feat: complete index.js 03-files-in-folder

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,13 @@
+const fs = require('node:fs');
+const path = require('node:path');
+const secretFolder = path.resolve(__dirname, 'secret-folder');
+const files = fs.readdir(secretFolder, {withFileTypes: true}, (_, files) => {
+	for(const file of files) {
+		const filePath = path.resolve(__dirname, `secret-folder/${file.name}`);
+		if (file.isFile()) {
+			fs.stat(filePath, (err, stats) => {
+				console.log(path.basename(file.name,path.extname(file.name)) + ' - ' + path.extname(file.name).replace('.','') + ' - ' + `${Number(stats.size / 1024).toFixed(3)}kb`);
+			})
+		}
+	}
+});
